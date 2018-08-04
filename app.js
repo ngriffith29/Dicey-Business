0fd52b125ef4b81d44e9149222a7c331037f5bda
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-04 15:42:49 -0400

    Got my divs printing the correct random number

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 $(document).ready (function (){
-    
-   class 
+  
+
+
+   class Die {
+       constructor(value){
+        this.value = value
+       }
+
+       roll(){
+       this.value = Math.floor(Math.random() * 6) + 1; 
+       return this.value
+       $('div').addClass(`${this.value}`)
+       }
+
+
+   }
+
+   let roll = new Die()
+roll.roll()
+
+
+
+let div = $("<div/>").text("Hello World")
+
+   $('#button').on("click",function (){
+       $('<div/>',{
+           'class': `div ${roll.roll()}`,
+           text: 'CLICK ME'
+       }).appendTo('body');
+})
+
+
+
 
 
 
