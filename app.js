4269063113a9adb9ce9ac80097e42d018275cda1
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-04 15:59:40 -0400

    Updated index and css

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ $(document).ready (function (){
        constructor(value){
         this.value = value
        }
-
+       //Methods go under here
        roll(){
        this.value = Math.floor(Math.random() * 6) + 1; 
        return this.value
        $('div').addClass(`${this.value}`)
        }
 
-
    }
-
+//test classes and methods here
    let roll = new Die()
 roll.roll()
 
-
+//other
    $('#button').on("click",function (){
        $('<div/>',{
            'class': `div ${roll.roll()}`,
