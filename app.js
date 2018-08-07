42acd460657da3b217fb78a86d501b9b16ce8ecb
Nathan Griffith <ngriffith29@student.egcc.edu>
2018-08-07 13:27:47 -0400

    broke

--- app.js
+++ app.js
@@ -1,65 +1,61 @@
 
 let dieVals = []
-let sum = 0 
+let sum = 0
 let count = 0
 
 
 class Die {
-    constructor(value){
+    constructor(value) {
         this.value = value
+        
+
+
+        $('<div/>').appendTo('body');
         this.roll()
-        this.div(this.value,this.value)
-       
-       
+        $("div").on("dblclick", function () {
+            $(this).remove()
 
 
-        
-        
+
+        })
+        $("div").click(function () {
+            this.roll()
+        })
+
+
+
+
+
+
+
     }
-    roll(){
-        this.value = this.randomNum(6,1)
-       dieVals.push(`${this.value}`) 
-     
-      
-       
-      
-    }    
-    div(cl, text){
+    roll() {
+        this.value = this.randomNum(6, 1)
+        $("div").text(`${this.value}`)
         
-        $('<div/>', {
-            'class': `${count++}`,
-            text: `${text}`
-        }).appendTo('body');
-        $("div").on("dblclick",function (){
-            $(this).remove()    
-           
-                         
-        })
 
-        $('div').on("click", function (){
-            console.log("hi")
-        })
 
-        
-        
+
 
     }
 
-    randomNum(num1,num2){
-      return  Math.floor(Math.random() * num1) + num2;
+
+    randomNum(num1, num2) {
+        return Math.floor(Math.random() * num1) + num2;
     }
 }
 //
 
-$("#button").on("click", function (){
+$("#button").on("click", function () {
     let d1 = new Die()
 })
 
-$("#sum").on("click", function (){
-    let sum = dieVals.reduce(function(a,b){
-        return Number(a)+Number(b);})
-        alert(sum)
-       
+$("#sum").on("click", function () {
+    let sum = dieVals.reduce(function (a, b) {
+        return Number(a) + Number(b);
+    })
+    alert(sum)
+
 })
 //
 
